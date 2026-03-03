9d9c9db049ff785c268d7630855f7a34a789e3f3
quals21 <quals21@gmail.com>
2026-03-03 13:46:16 +0530

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('quals21-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
