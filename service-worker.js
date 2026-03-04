06e7169e14a9af05912c2977bdd6b28f7a8d897d
quals21 <quals21@gmail.com>
2026-03-04 14:49:36 +0530

    Update service worker to new cache version and structure

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,39 @@
-const CACHE_NAME = "quals21-cache-v2";
+const CACHE_NAME = "quals21-cache-v4";
 
-const urlsToCache = [
-  "/quals21-pwa/",
-  "/quals21-pwa/index.html",
-  "/quals21-pwa/icon-192.png",
-  "/quals21-pwa/icon-512.png"
-];
-
-self.addEventListener("install", event => {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cache) {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
   );
 });
